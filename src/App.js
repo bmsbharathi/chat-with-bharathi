245eeb3a4284be681a1a375611986bffd309eb03
Bharathi BMS <bmsbharathi@gmail.com>
2021-06-21 00:35:06 +0530

    Restructure components to add go to home redirect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "firebase/firestore";
 import "firebase/auth";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt, faSignInAlt, faUsers, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt, faSignInAlt, faHome, faUsers, faUser } from '@fortawesome/free-solid-svg-icons';
 import './css/App.css';
 import Chatroom from './Chatroom';
 import Chat from './Chat';
@@ -60,12 +60,13 @@ function App() {
     <div className="App" >
       <div className="header">
         <b className="pageTitle">Chat with Bharathi</b>
-        <FontAwesomeIcon icon={faSignOutAlt} style={{ display: user ? "block" : "none" }} className="fa-3x signout icon" onClick={logout} />
+        <FontAwesomeIcon icon={faSignOutAlt} style={{ display: user ? "block" : "none" }} className="fa-3x icon logout" onClick={logout} />
+        <FontAwesomeIcon icon={faHome} style={{ display: (user && (showGroupChat || showPersonalChat)) ? "block" : "none" }} className="fa-3x icon logout" onClick={() => { setShowGroupChat(false); setShowPersonalChat(false); }} />
       </div>
       <div className="welcomeMessage" style={{ display: user ? "none" : "block" }}>
         <p>
           This App is created to apply my reacting and integrate it with Firebase for the backend.<br />
-          Play around with it and let me know if you like it. Cheers!
+          Play around with it and let me know if you like it.Cheers!
         </p>
         <FontAwesomeIcon icon={faSignInAlt} className="fa-4x icon" onClick={loginWithGoogle} />
         <br />Sign in with Google
@@ -79,9 +80,9 @@ function App() {
 
         <h4>Here you can do two things</h4> <br />
         <p>
-          Chat with Bharathi &nbsp;<FontAwesomeIcon icon={faUser} className="fa-2x icon" onClick={() => openService('chat')} /> <br />
+          Chat with Bharathi &nbsp; <FontAwesomeIcon icon={faUser} className="fa-2x icon" onClick={() => openService('chat')} /> <br />
           <br />
-          Go to Chatroom &nbsp;<FontAwesomeIcon icon={faUsers} className="fa-2x icon" onClick={() => openService('chatroom')} /> <br />
+          Go to Chatroom &nbsp; <FontAwesomeIcon icon={faUsers} className="fa-2x icon" onClick={() => openService('chatroom')} /> <br />
         </p>
       </div>
       <div className="serviceArea" style={{ display: (showGroupChat || showPersonalChat) ? "block" : "none" }}>
--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,16 @@
-import './css/Home.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
-import 'firebase/auth';
-import Chat from './Chat';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import 'firebase/firestore';
-
-const Home = ({ firebase }) => {
-
-    const auth = firebase.auth();
-    const [user] = useAuthState(auth);
-
-    const AuthWithGoogle = () => {
-
-        const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(googleAuthProvider);
-    };
-
-
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from './App';
+const Home = () => {
     return (
         <div>
-            {user == null ? <div className="home">
-                <h1 className="pageTitle">Chat with Bharathi</h1>
-                <div className="description">
-                    <span>
-                        This App is created to apply my reacting and integrate it with Firebase for the backend.<br />
-                        Play around with it and let me know if you like it. Cheers!
-                    </span>
-                </div>
-
-                <div className="login">
-                    <FontAwesomeIcon icon={faSignInAlt} className="fa-5x icon" onClick={AuthWithGoogle} />
-                    <br />Sign in with Google
-                </div>
-            </div> : <Chat firebase={firebase} />}
-        </div>
-    );
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <App />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>);
 }
+
 export default Home;
