cc67299e418cf6feb4e031b86509c78e1ad628f5
Bharathi BMS <bmsbharathi@gmail.com>
2021-06-20 21:03:03 +0530

    Fix alignment of messages displayed in chatroom

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -4,6 +4,8 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import "firebase/firestore";
 import "firebase/auth";
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import "./css/Chatroom.css";
+import { DEFAULT_CHAT_DP } from "./ENV_CONSTANTS";
 
 
 const Chatroom = (props) => {
@@ -29,6 +31,7 @@ const Chatroom = (props) => {
         var messageDocument = {
             message: messageInput,
             sender: loggedInUser.displayName,
+            senderUid: loggedInUser.uid,
             timestamp: new Date(),
             displayPhoto: loggedInUser.photoURL
         };
@@ -47,12 +50,24 @@ const Chatroom = (props) => {
 
     return (
         <div className="chatroom">
-
             <h2>Chat Room</h2>
-            <div>
+            <div className="chatroomMessages">
                 {chatroomMessages && chatroomMessages.map(
                     (messageItem) => {
-                        return (<p key={messageItem.id}>{messageItem.message}</p>)
+                        var sentTime = messageItem.timestamp.toDate().toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
+                        var align = "left";
+                        if (messageItem.senderUid === loggedInUser.uid) {
+                            align = "right";
+                        }
+                        return (
+                            <div className="message" key={messageItem.id}>
+                                <img align={align} className="displayPicture" src={messageItem.displayPhoto ? messageItem.displayPhoto : DEFAULT_CHAT_DP} alt="displayPicture" />
+                                <p align={align} className="content">
+                                    {messageItem.message}<br />
+                                    {sentTime}
+                                </p>
+                            </div>
+                        )
                     }
                 )}
             </div>
@@ -62,7 +77,7 @@ const Chatroom = (props) => {
                     <FontAwesomeIcon icon={faPaperPlane} className="fa-2x icon" onClick={sendMessage} />
                 </form>
             </div>
-        </div>
+        </div >
     );
 }
 
