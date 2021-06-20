67c10f30462d91a10ae601d183f290377732f309
Bharathi BMS <bmsbharathi@gmail.com>
2021-06-21 00:36:33 +0530

    Add ENV_constants.js

---
+++ src/ENV_CONSTANTS.js
@@ -0,0 +1,10 @@
+export const ADMIN_UID = "";
+export const API_KEY = "";
+export const AUTH_DOMAIN = "";
+export const DATABASE_URL = "";
+export const PROJECT_ID = "";
+export const STORAGE_BUCKET = "";
+export const MESSAGING_SENDER_ID = "";
+export const APP_ID = "";
+export const MEASUREMENT_ID = "";
+export const DEFAULT_CHAT_DP = "./chat_dp.ico";
